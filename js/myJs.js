0c0fbb89ae29fd130e0697c27cf16f47f48595ab
JOSU <kamalnetra@702gmail.com>
2021-04-28 11:40:11 +0530

    finished dance animation

--- js/myJs.js
+++ js/myJs.js
@@ -1,29 +1,25 @@
 var stateBulb = false;
 let buttonArray = document.getElementsByClassName("buttons");
 function turnOn() {
-    var tubelightArray = document.getElementsByClassName("tubelight-container")[0];
+    var tubelight = document.getElementsByClassName("tubelight")[0];
     var btn = document.getElementsByClassName("buttons")[0];
     if (stateBulb) {
-        for (var i = 0; i < tubelightArray.length; i++) {
-            tubelightArray[i].style.backgroundColor = "rgb(53, 53, 53)";
-        }
+        tubelight.style.backgroundColor = "rgb(53, 53, 53)";
         stateBulb = false;
         btn.innerHTML = "Turn on";
-        turnOffHandRotate();
         enableAllButtons();
     } else {
-        for (var i = 0; i < tubelightArray.length; i++) {
-            tubelightArray[i].style.backgroundColor = "#FAE844";
-        }
+        tubelight.style.backgroundColor = "#FAE844";
         stateBulb = true;
         btn.innerHTML = "Turn off ";
-        turnOnHandRotate();
         disableAllOtherButton(btn);
     }
 }
 
 
 function hiAnimation() {
+    var btn=document.getElementsByClassName("buttons")[1];
+    disableAllOtherButton(btn);
 
     var head = document.getElementsByClassName("and-head")[0];
     var lEye = document.getElementsByClassName("l-eye")[0];
@@ -49,23 +45,43 @@ function hiAnimation() {
         lHand.style.animation = "";
         rHand.style.animation = "";
         rleg.style.animation = "";
+        enableAllButtons();
     });
 }
 
-function turnOnHandRotate() {
+function danceAnimation() {
+    var btn=document.getElementsByClassName("buttons")[2];
+    disableAllOtherButton(btn);
+
+    var head = document.getElementsByClassName("and-head")[0];
+    var lEye = document.getElementsByClassName("l-eye")[0];
+    var rEye = document.getElementsByClassName("r-eye")[0];
+    var body = document.getElementsByClassName("and-body")[0];
+    var lHand = document.getElementsByClassName("l-hand")[0];
     var rHand = document.getElementsByClassName("r-hand")[0];
-    rHand.animate(
-        [
-            {
-                transformOrigin: "top"
-            }
-            , {
-                transform: "rotate(-65deg)"
-            }
-        ], {
-        duration: 900, iterations: 1, fill: 'forwards'
-    }
-    );
+    var rleg = document.getElementsByClassName("r-leg")[0];
+    var lleg = document.getElementsByClassName("l-leg")[0];
+
+    head.style.animation = "3s dance-head-movement ease-in-out 0s 4 alternate";
+    lEye.style.animation = "3s dance-eye-movement 0s 4 alternate";
+    rEye.style.animation = "3s dance-eye-movement 0s 4 alternate";
+    body.style.animation = "3s dance-body-movement ease-in-out 0s 4 alternate";
+    lHand.style.animation = "3s dance-lhand-movement ease-in-out 0s 4 alternate";
+    rHand.style.animation = "3s dance-rhand-movement ease-in-out 0s 4 alternate";
+    rleg.style.animation = "1.5s dance-rleg-movement ease-in-out 0s 8 alternate";
+    lleg.style.animation = "1.5s dance-lleg-movement ease-in-out 0s 8 alternate";
+
+    head.addEventListener("animationend", () => {
+        head.style.animation = "";
+        lEye.style.animation = "";
+        rEye.style.animation = "";
+        body.style.animation = "";
+        lHand.style.animation = "";
+        rHand.style.animation = "";
+        rleg.style.animation = "";
+        lleg.style.animation = "";
+        enableAllButtons();
+    });
 }
 
 function turnOffHandRotate() {
@@ -112,3 +128,4 @@ function enableAllButtons() {
         buttonArray[i].style.display = "flex";
     }
 }
+
